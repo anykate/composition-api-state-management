f265d5a7f27ceed042bd02c00abe1e4e7c6779fd
Aniket Aryamane <aryamane.aniket@gmail.com>
2023-09-21 23:54:53 +0530

    Adding logout functionality

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,14 +1,34 @@
 <script setup>
 import useUserState from '@/store/useUserState'
 
-const { getFullName } = useUserState()
+const { getFullName, updateIsLoggedIn } = useUserState()
 const fullName = getFullName()
 </script>
 
 <template>
-    <div>
+    <div class="row">
         <h3>User: {{ fullName }}</h3>
+        <button
+            type="submit"
+            class="btnLogout"
+            @click="updateIsLoggedIn(false)"
+        >
+            Logout
+        </button>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.btnLogout {
+    margin: 15px 0;
+    width: fit-content;
+    padding: 8px;
+    cursor: pointer;
+}
+.row {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 1rem;
+}
+</style>
